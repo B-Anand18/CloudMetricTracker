b0ee0b85829319526f702d60ae7eb8c49ed2d9e4
B Anand <81760438+B-Anand18@users.noreply.github.com>
2024-02-18 17:59:58 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import psutil
+from flask import Flask, render_template
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+
+    interval = 1
+    cpu_percent = psutil.cpu_percent(interval = interval) * 10
+    mem_percent = psutil.virtual_memory().percent
+    message = None
+
+    if cpu_percent > 80 or mem_percent > 80:
+        message = "High CPU or Memory Utilization detected!!!  Please Scale up"
+
+    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
